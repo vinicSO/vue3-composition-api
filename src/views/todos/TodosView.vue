4d9cfe047efbdd70da3d1bf4d107dec6df1dc611
Vinícius Oliveira <vinicius.oliveira@engeselt.com.br>
2023-03-12 15:02:54 -0300

    indicate loading data

--- src/views/todos/TodosView.vue
+++ src/views/todos/TodosView.vue
@@ -1,6 +1,8 @@
 <template>
     <h1>Lista de Tarefas</h1>
 
+    <div v-if="loading">Carregando as tarefas</div>
+
     <ul>
         <li v-for="todo in todos" :key="todo.identify">{{ todo.title }}</li>
     </ul>
@@ -17,15 +19,23 @@
         setup () {
 
             const todos = ref([])
+
+            const loading = ref(false)
             
             onMounted(() => {
+
+                loading.value = true
+
                 TodoService.getAll()
-                .then(response => todos.value = response.data.data)
-                .catch(error => console.log(error))
+                    .then(response => todos.value = response.data.data)
+                    .catch(error => console.log(error))
+                    .finally(() => loading.value = false)
+
             })
             
             return {
-                todos
+                todos,
+                loading,
             }
         }
     }
